cb6c77e0c6e6e6c9fc9b554c3892bd85354582fd
Artem <70816250+Flajen@users.noreply.github.com>
2020-09-13 02:14:56 +0300

    Update script.js

--- Shop/js/script.js
+++ Shop/js/script.js
@@ -1,8 +1,16 @@
-// ==========================================
-var num = 1;
-let text = "товар";
+// Переменные
+var num = 1; // Кол-во корзины
+let text = "товар"; // Правильность написания
+let amount = 1; // Кол-во товара
+let price = 132;
 
-function change() {
+// Кнопки
+const btn = document.querySelector(".button");
+const cart = document.querySelector(".cart_mouse");
+const products = document.querySelector(".product_img");
+
+// Функции
+function change() { // Изменения товара
     document.querySelector(".place").innerHTML = num++ + " " + text;
     if (num == 1) {
 		text;
@@ -13,19 +21,24 @@ function change() {
 	};
 };
 
-const btn = document.querySelector(".button");
-
-btn.addEventListener("click", function() {
-	change();
-});
-
-
-function add_cart() {
+function add_cart() { // Показ корзины
 	document.getElementById("showing").classList.toggle("showing");
-
 }
 
-let cart = document.querySelector(".cart_mouse");
+function add_product() { // Выбор продука
+	document.getElementById("product_img").classList.add("product_img_active");
+}
+
+// Клик
+btn.addEventListener("click", function() {
+	change();
+
+	document.querySelector(".cart_heading").innerHTML = "«North Blue» Полированная 600/400";
+
+	document.querySelector(".cart_amount").innerHTML = "Количество: " + amount++;
+
+	document.querySelector(".cart_price").innerHTML = "Цена: " + price++ + " руб.";
+});
 
 cart.addEventListener("click", function() {
 	if (num == 1) {
@@ -34,5 +47,26 @@ cart.addEventListener("click", function() {
 	};
 });
 
+products.addEventListener("click", function() {
+	add_product();
+});
 
-// =========
+
+var products4343 = {
+	"1" : {
+		"name" : "«North Blue» Полированная",
+		"amount" : 15,
+		"cost" : 1088.00,
+		"img" : "img/product/North_Blue_polished_400x400.png",
+	},
+};
+
+var out = '';
+for (var key in products4343) {
+
+};
+
+var show = function (state) {
+	document.getElementById("checkout").style.display = state;
+	document.getElementById("filter").style.display = state;
+};

403532997aab76c27604b12bd70c194b54508398
Artem <70816250+Flajen@users.noreply.github.com>
2020-09-22 06:08:04 +0300

    Update script.js

--- Sointem/js/script.js
+++ Sointem/js/script.js
@@ -4,4 +4,30 @@
 
 // Скрипты
 
+// Переключение вкладок
+$('#cw_one').click( function() {
+	if ($('#cw_one').hasClass('active')) {
+	} else {
+		$('.cw_text').removeClass('active');
+		$('#cw_one').addClass('active');
+		$('#cw__work_one').attr('checked', true);
+		$('#cw__work_two').removeAttr('checked');
+	}
+})
+
+$('#cw_two').click( function() {
+	if ($('#cw_two').hasClass('active')) {
+	} else {
+		$('.cw_text').removeClass('active');
+		$('#cw_two').addClass('active');
+		$('#cw__work_two').attr('checked', true);
+		$('#cw__work_one').removeAttr('checked');
+	}
+})
+
 // Проверка работы скрипта
+if ($('#cw_one').is(':checked')) {
+	console.log('Да');
+} else {
+	console.log('Нет');
+}

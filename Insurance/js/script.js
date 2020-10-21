10d9f68d581fee2d6000a08f997f493895a2fc5c
Flajen <Mr.Flajen@gmail.com>
2020-10-22 00:22:56 +0300

    change

--- Insurance/js/script.js
+++ Insurance/js/script.js
@@ -1,42 +1,64 @@
-// Шапка
-$(window).scroll(function() {
-	if ($(this).scrollTop() > 100){
-		$('.header_cap').addClass("cap_active_none");
-
-		if ($(this).scrollTop() > 121) {
-			$('.header_cap').addClass("cap_active_animation");
-
-			if ($(this).scrollTop() > 264) {
-				$('.header_cap').addClass("cap_active");
-			}
-
-			else {
-				$('.header_cap').removeClass("cap_active");
-			}
-		}
-
-		else {
-			$('.header_cap').removeClass("cap_active_animation");
-		}
-	}
-
-	else{
-		$('.header_cap').removeClass("cap_active_none");
-		$('.header_cap').removeClass("cap_active_animation");
-	}
-});
-
 // Вставка текста по клику
 $('#read_changing').click(function() {
 	let textExpample = $('#example_changing').html()
 	$('#write_changing').val(textExpample);
 })
 
+// Выбор элемента
+	// Переменные
+var input = $('.hf_field');
+var personal = $('#personal');
+var personalBlock = $('.personal_block');
+	// Функция
+input.click(function() {
+	input.addClass('hff_active');
+});
+	// Функция
+personal.click(function() {
+	if (personalBlock.css('display') == 'block') {
+		personalBlock.css('display', 'none');
+	}else {
+		personalBlock.css('display', 'block');
+	}
+});
+	// Скрытие при клике на область помимо самого блок
+$(document).click(function (e){ // событие клика по веб-документ
+	// если клик был не по нашему блоку
+		if (!input.is(e.target) && input.has(e.target).length === 0) { // и не по его дочерним элементам
+			input.removeClass('hff_active');
+		}
+		if (!personalBlock.is(e.target) && !personal.is(e.target) && personalBlock.has(e.target).length === 0) {
+			personalBlock.css('display', 'none');
+		}
+});
+
+
 // Список из примеров
 let	example_list = ["нужно застраховать дом", "нужно застраховать машину", "застраховать себя", "застраховать собаку"];
 
 // Загрузка документа
 $( document ).ready(function() {
+	// Шапка
+		// Переменные
+	var cap = $('.header_cap'),
+	scrollPrev = 0;
+		// Функция
+	$(window).scroll(function() {
+		var scrolled = $(window).scrollTop();
+	 
+		if ( scrolled > 1 && scrolled > scrollPrev ) {
+			cap.addClass('cap_active_none');
+			cap.css('box-shadow', 'none');
+			personalBlock.css('display', 'none');
+		} else {
+			cap.removeClass('cap_active_none');
+			cap.css('box-shadow', '0 0 10px 0.1px #222');
+		}
+		if (scrolled == 0) {
+			cap.css('box-shadow', 'none');
+		}
+		scrollPrev = scrolled;
+	});
 
 	// Замена текста из массива
 		// Переменные
@@ -50,33 +72,39 @@ $( document ).ready(function() {
 	$('#example_changing').html(example_list[randomNumber]);
 
 	// Слайдер
-	$('.owl-carousel').owlCarousel({
-		// Показывать слайды покругу
-		loop: true,
-
-	  	// Отступ
-	  	margin: 15,
-
-	  	// Автоматическое проигрывание
-	  	autoplay: true,
-	  	autoplayTimeout: 3000,
-	});
-	$('#js-carousel-1').each( function() {
-  		items: 3 // А по-умолчанию 3
-  		// Создаем карусель
-	    var owl = $(this).find('.owl-carousel').owlCarousel();
-
-	  	// При клике по кнопке Влево
-	  	$(this).find('.js-prev').on('click', function () {
-	    // Перематываем карусель назад
-	    	owl.trigger('prev.owl.carousel');
-	  	});
-
-	  	// При клике по кнопке Вправо
-	  	$(this).find('.js-next').on('click', function () {
-	    	// Перематываем карусель вперед
-	    	owl.trigger('next.owl.carousel');
-	  	});
-	});
-
+	$('.slider').slick({
+		centerMode: true,
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 800,
+		easing: 'easeOutSine',
+		adaptiveHeight:true,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		centerMode: true,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		touchThreshold: 15,
+		// waitForAnimate: false, Быстрый переход(откл анимации)
+		centerPadding: '60px',
+		responsive: [{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 3
+				}
+			}, {
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1
+				}
+			}]
+		});
 });

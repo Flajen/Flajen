7b33c6c992db6a22098ea5b117e0c571c274d3a3
Flajen <mr.flajen@gmail.com>
2021-04-19 23:54:32 +0300

    delete

--- WayUp/js/main.js
+++
@@ -1,9 +0,0 @@
-var btn = document.getElementById('btn');
-
-btn.onclick = function () {
-	var text = document.querySelector('.ofters');
-	text.classList.add('red');
-	var img = document.querySelector('.desktop');
-	img.style.display = 'none';
-	document.querySelector('.rocket').style.marginLeft = '50px';
-}

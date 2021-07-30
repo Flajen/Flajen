b5a99e0bafad24dd450e1d45e5147c8976409af8
Flajen <mr.flajen@gmail.com>
2021-07-30 05:34:59 +0300

    change

--- arbitr/js/calendar.js
+++ arbitr/js/calendar.js
@@ -12,4 +12,13 @@ $(document).ready(function(){
   $('.calendar-popup-close').click(function(){
     $('.calendar-popup').hide();
   });
+  $('.calendar-btn').click(function(){
+    if (!$('.calendar_write-input-check').val() == '') {
+      $('.calendar-card-block').removeClass('calendar-card-block-inactive').addClass('calendar-card-block-inactive');
+      $('.calendar-card-block-number').addClass('last-month');
+      $('.calendar-card-block-info').addClass('calendar-card-block-text').removeClass('calendar-card-block-info').html(`
+        Информация о заседаниях<br>отсутствует
+        `);
+    }
+  });
 });

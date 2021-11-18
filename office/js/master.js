b29581b54fe6964b1a0d7e2ec74343ff6a7dba88
Flajen <mr.flajen@gmail.com>
2021-11-18 13:51:17 +0300

    change

--- office/js/master.js
+++ office/js/master.js
@@ -8,11 +8,28 @@ $(document).ready(function(){
     })
     $('.nav-flex').hide();
     off = true;
-  }
+  };
+  function openPopup(){
+    $('.additionally-blackout').show();
+    $('.additionally-popup').show();
+  };
+  function closePopup(){
+    $('.additionally-blackout').hide();
+    $('.additionally-popup').hide();
+    $('.additionally-video').attr('src', '');
+  };
+  $('.video').click(function(){
+    var th = $(this);
+    $('.additionally-video').attr('src', th.find('img').attr('data-link'));
+    openPopup();
+  });
+  $('.additionally-blackout').click(function(){
+    closePopup();
+  });
   if (window.location.hash == '#directions') {
     $('html, body').animate({ scrollTop: $('#directions').offset().top - 100 }, 600);
     window.location.hash = '';
-  }
+  };
   $("body").on('click', '[href*="#"]', function(e){
     var fixed_offset = 70;
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
@@ -35,5 +52,5 @@ $(document).ready(function(){
     } else {
       close_nav();
     }
-  })
+  });
 });

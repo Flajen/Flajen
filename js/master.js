c1d4dd23d352f8fd38b26cb5c4584f67ecfdddc6
Skepsite <70816250+Flajen@users.noreply.github.com>
2022-02-24 17:30:34 +0300

    change
    v 0.1.7

--- js/master.js
+++ js/master.js
@@ -27,10 +27,11 @@ $(document).ready(function(){
     var fixed_offset = 100;
     $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
     e.preventDefault();
-    if ($('.header_nav-link').is(e.target)) {
+    if ($('.header_nav-link').is(e.target) && window.innerWidth < 993) {
       closeMenu()
     }
   });
+  console.log(window.innerWidth);
   $('.header_mobile-btn').click(function(){
     if ($('header').hasClass('open')) {
       closeMenu()
@@ -40,6 +41,7 @@ $(document).ready(function(){
       $('header').addClass('open');
     }
   })
+
   var blockOptionBtn = $('.home_block-option-btn'),
       blockOption = $('.home_block-option');
   blockOptionBtn.click(function(){
@@ -48,7 +50,6 @@ $(document).ready(function(){
       thParent.removeClass('open');
       thParent.find('.home_block-select-lists').removeClass('open');
     } else{
-      console.log("Нет");
       blockOptionBtn.parent().removeClass('open');
       blockOptionBtn.parent().find('.home_block-select-lists').removeClass('open');
       thParent.addClass('open');
@@ -60,6 +61,7 @@ $(document).ready(function(){
       thParent.removeClass('open');
     })
   })
+
   // $('body').mouseup(function (e){
   //   var div = $('.home_block-option-btn');
   //   // if (!div.is(e.target) && div.has(e.target).length === 0 && blockOptionBtn.is(e.target)) {
@@ -109,7 +111,9 @@ $(document).ready(function(){
         },
         pagination: false,
       },
-    }
+    },
+    // resistance: false,
+    resistanceRatio: 0.6,
   });
   const jobsPopupSwiper = new Swiper('.jobs_popup_swiper', {
     slidesPerView: 1,
@@ -140,6 +144,7 @@ $(document).ready(function(){
     observeParents: true,
     observeSlideChildren: true,
     observer: true,
+    resistanceRatio: 0.6,
   });
   var ind = 0;
   $('button[name="slide"]').each(function(){
@@ -151,13 +156,14 @@ $(document).ready(function(){
       $('.jobs_popup-wrapper').addClass('open');
       // disabledScroll();
       var elem = parseInt($(this).attr('name'))-1;
-      jobsPopupSwiper.slideTo(elem, 1, false);
+      jobsPopupSwiper.slideTo(elem, 0);
     }
   })
   new Swiper('.price_table_swiper', {
     slidesPerView: 'auto',
     freeMode: false,
     centeredSlides: false,
+    resistanceRatio: 0.1,
   });
   new Swiper('.cooperation_swiper', {
     slidesPerView: 'auto',
@@ -176,7 +182,8 @@ $(document).ready(function(){
         spaceBetween: 25,
         speed: 700,
       },
-    }
+    },
+    resistanceRatio: 0.5,
   });
   $('.video-one').click(function(){
     $('.video_popup-one').addClass('open')

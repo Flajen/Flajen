a0e36315a8b6ed413af998999a9d96a125734962
Skepsite <70816250+Flajen@users.noreply.github.com>
2022-09-20 05:53:16 +0300

    change
    v 0.0.2

--- zizhu/js/master.js
+++ zizhu/js/master.js
@@ -19,11 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
           if (burger.classList.contains("open")) {
             burger.classList.remove("open");
             headerMoblie.classList.remove("open");
-            // headerMoblie.classList.add("white");
+            if (headerMoblie.classList.contains("up") && window.scrollY == 0) {
+              headerMoblie.classList.add("white");
+            }
           } else {
             burger.classList.add("open");
             headerMoblie.classList.add("open");
-            // headerMoblie.classList.remove("white");
+            headerMoblie.classList.remove("white");
           }
         });
       });
@@ -48,7 +50,9 @@ document.addEventListener("DOMContentLoaded", () => {
           headerMoblie.classList.remove("white");
         } else {
           headerMoblie.classList.remove("fly");
-          headerMoblie.classList.add("white");
+          if (headerMoblie.classList.contains("up")) {
+            headerMoblie.classList.add("white");
+          }
         }
       });
       if (window.scrollY > 0) {
@@ -57,4 +61,54 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }
   }
+  const btnRequest = document.querySelectorAll(".btnRequest");
+  const popups = document.querySelectorAll(".popup");
+  const popupRequest = document.querySelector(".popupRequest");
+  const popupSuccess = document.querySelector(".popupSuccess");
+  var check = false;
+  let disableScroll = function () {
+    let pagePosition = window.scrollY;
+    let scroll = window.innerWidth - document.documentElement.clientWidth;
+    document.body.classList.add("disable-scroll");
+    document.body.dataset.position = pagePosition;
+    document.body.style.top = -pagePosition + "px";
+    document.body.style.paddingRight = scroll + "px";
+    popups.forEach((popup) => {
+      popup.style.paddingRight = 20 + scroll + "px";
+    });
+    check = true;
+  };
+  let enableScroll = function () {
+    if (check) {
+      let pagePosition = parseInt(document.body.dataset.position, 10);
+      document.body.style.top = "auto";
+      document.body.classList.remove("disable-scroll");
+      window.scroll({ top: pagePosition, left: 0 });
+      document.body.removeAttribute("data-position");
+      document.body.style.paddingRight = 0;
+      popups.forEach((popup) => {
+        popup.style.paddingRight = 20 + "px";
+      });
+    }
+    check = false;
+  };
+  btnRequest.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      popupRequest.classList.add("open");
+      popupRequest.classList.add("show");
+      disableScroll();
+    });
+  });
+  popups.forEach((popup) => {
+    popup.addEventListener("click", (e) => {
+      if (
+        e.target.classList.contains("popup") ||
+        e.target.classList.contains("popup-close")
+      ) {
+        popup.classList.remove("show");
+        popup.classList.remove("open");
+        enableScroll();
+      }
+    });
+  });
 });
--- zizhu/js/scroll.js
+++ zizhu/js/scroll.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   $("body").on("click", '[href*="#"]', function (e) {
-    var fixed_offset = 150;
+    if (window.innerWidth > 1220) {
+      var fixed_offset = $(".header-fly").outerHeight();
+    } else {
+      var fixed_offset = $(".header-mobile").outerHeight();
+    }
     $("html,body")
       .stop()
-      .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+      .animate(
+        { scrollTop: $(this.hash).offset().top - (fixed_offset + 20) },
+        1000
+      );
     e.preventDefault();
   });
 });
